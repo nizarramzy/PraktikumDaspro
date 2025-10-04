458144f4e38dbf6ab4fde64c19aac58f57648bcb
nizarramzy <nizarramzy07@gmail.com>
2025-10-04 14:44:57 +0700

    revisi

---
+++ src/jobsheet5/ifCetakKRS22.java
@@ -0,0 +1,23 @@
+package jobsheet5;
+import java.util.Scanner;
+public class ifCetakKRS22 {
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS Siakad ---");
+        System.out.println("Apakah UKT Sudah Lunas? (True/false?) ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas){
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else  {
+            System.out.println("Gagal! silahkan lunasi UKT terlebih dahulu");
+
+        } 
+        
+        sc.close();
+        
+     }
+}
