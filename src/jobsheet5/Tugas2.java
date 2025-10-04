725beab35faa7f5b37679d0764ef569ba4d82b7b
nizarramzy <nizarramzy07@gmail.com>
2025-10-04 14:45:35 +0700

    Tugas 2

---
+++ src/jobsheet5/Tugas2.java
@@ -0,0 +1,23 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class Tugas2 {
+      
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Ganjil");
+            
+        } else {
+            System.out.println("Genap");
+        }
+
+        sc.close();
+
+        
+    }
+}
