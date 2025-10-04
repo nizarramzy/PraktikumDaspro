88f9974cd36658645bd9c1cbdf1b69a3f586e58e
nizarramzy <nizarramzy07@gmail.com>
2025-10-04 13:02:44 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS22.java
+++ jobsheet5/ifCetakKRS22.java
@@ -9,14 +9,15 @@ public class ifCetakKRS22 {
         System.out.println("Apakah UKT Sudah Lunas? (True/false?) ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas);
+        if (uktLunas){
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else  {
+            System.out.println("Gagal! silahkan lunasi UKT terlebih dahulu");
 
-
-
+        } 
         
-        
+        sc.close();
         
      }
 }
